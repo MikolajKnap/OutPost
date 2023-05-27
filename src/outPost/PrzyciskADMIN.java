fd5495b8aabfe6cfa4579703561fc0f9b7ec0d2a
fmarzecki <filipo085@icloud.com>
2023-05-27 22:03:51 +0200

    Dodalem tu troche elastycznosci

--- src/outPost/PrzyciskADMIN.java
+++ src/outPost/PrzyciskADMIN.java
@@ -7,6 +7,17 @@ import java.awt.Font;
 import javax.swing.JButton;
 
 public class PrzyciskADMIN extends JButton {
+
+    /*
+     * Ogolnie o co chodzi, bo jest tak ze nie trzeba tworzyc 
+     * JButton butonik = new JButton();
+     * a potem np.
+     * butonik.setBackground();
+     * 
+     * tylko robimy normalnie extends JButton i w konstruktorze 
+     * modyfikujemy sobie ten butonik, a potem tworzymy go tam gdzie chcemy
+     */
+    
     public PrzyciskADMIN() {
         setText("ADMIN");
         setBackground(PaczkomatGUI.kolorPrzyciskow);
