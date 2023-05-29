1965bfbc12005a754a6dbc17d066706f9d8eb6db
Mikolaj Knap <mikozxc@gmail.com>
2023-05-29 15:22:12 +0200

    Dodałem 2 panele do odbioru paczki, screeny dodam jak sie naucze tych fajnych screenow :)

---
+++ src/outPost/PanelOtwarciaSkrytki.java
@@ -0,0 +1,46 @@
+package outPost;
+
+import outPost.buttons.PrzyciskPowrot;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelOtwarciaSkrytki extends JPanel{
+    private JButton buttonPowrot;
+    private JLabel labelPlatnosc;
+    private JLabel labelPlatnosc2;
+
+    public PanelOtwarciaSkrytki() {
+        setLayout(new GridBagLayout());
+        setBackground(PaczkomatGUI.kolorTlaTytulu);
+
+        // Buttony
+        buttonPowrot = new PrzyciskPowrot();
+
+        // Powrot onClick
+        buttonPowrot.addActionListener(e -> {
+            PaczkomatGUI.ramka.remove(this);
+            PaczkomatGUI.ramka.add(PaczkomatGUI.panelGlowny);
+            PaczkomatGUI.ramka.revalidate();
+            PaczkomatGUI.ramka.repaint();
+        });
+
+        // Label z wyswietlona kwota do zaplaty
+        labelPlatnosc = new JLabel("Skrytka nr XX została otwarta");
+        labelPlatnosc.setFont(new Font(Font.DIALOG, Font.BOLD, 28));
+        labelPlatnosc.setHorizontalAlignment(JLabel.CENTER);
+
+        labelPlatnosc2 = new JLabel("Po 5 sekundach nastąpi automatyczne zamknięcie");
+        labelPlatnosc2.setFont(new Font(Font.DIALOG, Font.BOLD, 28));
+        labelPlatnosc2.setHorizontalAlignment(JLabel.CENTER);
+
+        // Ulozenie elementow na gridzie
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 0); // Ustawienie odstępów między przyciskami
+
+        add(labelPlatnosc, gbc); gbc.gridy = 1;
+        add(labelPlatnosc2, gbc); gbc.gridy = 2;
+        add(buttonPowrot, gbc);
+    }
+
+}
