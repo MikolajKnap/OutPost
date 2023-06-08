0e69dd9067784e609da6c0cd0a09c409cb83a240
Kamil Rojek <kamilrojek2002@gmail.com>
2023-06-08 14:46:54 +0200

    listener do serializacji

--- src/outPost/Ramka.java
+++ src/outPost/Ramka.java
@@ -6,7 +6,22 @@ import java.io.*;
 
 public class Ramka extends JFrame {
     public Ramka() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //listener nasłuchujący eventu zamknięcia okna X
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //serializacja sie dzieje
+                serializeData();
+                //właściwe zamkniecie okienka
+                System.exit(0);
+            }
+        });
         setSize(800, 600);
+    }
+
+    private void serializeData() {
+        System.out.println("listener działa ez, a MP nic nie wie co robi omg");
     }
 }
