fbbcd1bb5256506ee7e12a8822eb8369af7e4ecc
Kamil Rojek <kamilrojek2002@gmail.com>
2023-06-08 20:04:52 +0200

    serialize

--- src/outPost/Ramka.java
+++ src/outPost/Ramka.java
@@ -1,12 +1,20 @@
 package outPost;
 
+import outPost.paczki.Paczka;
+import outPost.PaczkomatGUI;
+
 import javax.swing.JFrame;
 import java.awt.event.*;
 import java.io.*;
+import java.util.ArrayList;
+
 
 public class Ramka extends JFrame {
     public Ramka() {
         //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        //String serializePaczkomaty = "../paczkomaty.ser";
 
         //listener nasłuchujący eventu zamknięcia okna X
         addWindowListener(new WindowAdapter() {
@@ -20,9 +28,36 @@ public class Ramka extends JFrame {
         });
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
+
     }
 
     private void serializeData() {
         System.out.println("listener działa ez, a MP nic nie wie co robi omg");
+    }
+
+    private void serializePaczki(ArrayList<Paczka> listaPaczek) {
+        String filePaczki = "../paczki.ser";
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filePaczki);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(listaPaczek);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void serializePaczkomaty(ArrayList<PaczkomatGUI> paczkomaty) {
+        String filePaczki = "../paczkomaty.ser";
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filePaczki);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(paczkomaty);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
